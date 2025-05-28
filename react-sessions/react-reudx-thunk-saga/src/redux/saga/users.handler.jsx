2000439eafa4a0cd8e31ced5256462c53fa35be9
deepakkumarshinde1 <deepakkumar.shinde1@gmail.com>
2025-05-28 08:48:39 +0530

    added try catch block

--- react-sessions/react-reudx-thunk-saga/src/redux/saga/users.handler.jsx
+++ react-sessions/react-reudx-thunk-saga/src/redux/saga/users.handler.jsx
@@ -3,6 +3,10 @@ import { getUsersService } from "./users.service";
 import { saveUsers } from "../users.slice";
 
 export function* getUsersHandler({ payload }) {
-  let { data } = yield call(getUsersService, payload);
-  yield put(saveUsers([data]));
+  try {
+    let { data } = yield call(getUsersService, payload);
+    yield put(saveUsers([data]));
+  } catch (error) {
+    console.log(error);
+  }
 }
